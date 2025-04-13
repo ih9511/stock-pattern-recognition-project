e2c99d40f09ac8f08af62010b246bcf3ca8fb185
Inheon Choi <cih956964@gmail.com>
2025-04-13 22:21:47 +0900

    refactor: 기존 리팩토링으로 인한 import 문 수정

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 """테이블 생성"""
-from app.database import engine, Base
-from app import models
+from backend.app.database import engine, Base
+from backend.app import models
 
 Base.metadata.create_all(bind=engine)
